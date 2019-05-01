b917e08a511447593a0830c0966c0606c3f7540f
Patrice Tisserand <p.tisserand@gmail.com>
2019-05-01 18:20:38 +0200

    java: added main class for TutorialMod

---
+++ src/main/java/com/pragmarob/minecraft/mod/tutorial/TutorialMod.java
@@ -0,0 +1,32 @@
+package com.pragmarob.minecraft.mod.tutorial;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid = TutorialMod.modId, name = TutorialMod.name, version = TutorialMod.version, acceptedMinecraftVersions = "[1.10.2]")
+public class TutorialMod {
+
+    public static final String modId = "tutorial";
+    public static final String name = "Tutorial mod";
+    public static final String version = "0.0.1";
+
+    @Mod.Instance(modId)
+    public static TutorialMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println(name + " is loading!");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
